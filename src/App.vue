d2e48380848f8fe7e9827cc63dde4b3dc69b3bbd
BT-BOX <852649348@qq.com>
2023-07-19 11:41:33 +0800

    第一次提交代码

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template></template>
+
+<script setup lang="ts">
+for (let i=0; i <10;i++)
+{
+    console.log(i)
+}
+</script>
+
+<style scoped>
+.box{
+  width: 600px;
+  height: 400px;
+  background: black;
+  h1{
+    color: white;
+  }
+}
+</style>
